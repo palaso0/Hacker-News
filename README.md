40eb4c284cffb223489885a35119aea96476114d
Paulo Choque <paulo.choque@ascbomacmini17.local>
2022-08-29 14:23:58 -0400

    Files added

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Hacker-News
-An app made in React for consume a GraphQL service.
+# Hacker- News
+Tutorial from `https://www.howtographql.com/react-apollo/0-introduction/`
+
+An app to add new web pages and their descriptions, search, pagination, login, etc.
+
+Made with: React, GraphQL and SQLite
+
+
+
+## Available Scripts
+
+For runing the project:
+
+### 1) Build the BD
+- Go to serve folder and enter: `yarn dev`
+
+
+### 2) Run the server
+- Stay on server forlder and enter `yarn start`
+Initialize the server on port 3000 [http://localhost:3000]
+
+### 2) Run the app
+- go back to main folder (Hacker-news) and enter `npm start`
+
---
+++ src/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from './constants';
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+
+
+// 1
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+// 3
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode >
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
